ef1fe7c0c60d9c77fcbba8be3b6a7c91b299e741
mijiFernandes <lkm366859@gmail.com>
2022-03-31 16:34:25 +0900

    Added files and changes required for deployment to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,9 @@ USE_L10N = True
 USE_TZ = True
 # USE_TZ = False
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
